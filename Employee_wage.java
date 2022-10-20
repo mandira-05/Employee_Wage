f301894a9042c7b64c09100e47f811f07ed54db1
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-20 15:51:57 +0530

    Added UC2 for Employee Wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,13 +1,17 @@
 public class Employee_wage {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
-
+        int Emp_Rate_Per_Hr = 20;
+        int empHrs = 0;
+        int empWage = 0 ;
+        
         double empCheck = Math.floor(Math.random()*10)%2;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
+            empHrs = 8;
         } else {
-            System.out.println("Employee is Absent");
+            empHrs = 0;
         }
-
+        empWage = empHrs * Emp_Rate_Per_Hr;
+        System.out.println("Emp Wage: " + empWage);
     }
 }
