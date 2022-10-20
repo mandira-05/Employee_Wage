feb6c8754e1519d30afad549176354e9997df707
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-20 16:29:28 +0530

    Added UC5 to calculate Employee wage for a month

--- Employee_wage.java
+++ Employee_wage.java
@@ -2,25 +2,30 @@ public class Employee_wage {
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
     public static final int Emp_Rate_Per_Hr = 20;
+    public static final int Num_of_working_day = 20;
 
     public static void main(String[] args) {
         int empHrs = 0;
-        int empWage = 0 ;
+        int empWage = 0;
+        int Total_empWage = 0;
 
-        int empCheck = (int) Math.floor(Math.random()*10)%3;
+        for (int day = 0; day < Num_of_working_day; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            Total_empWage=Total_empWage+empWage;
+            System.out.println("Emp Wage: " + empWage);
         }
-
-        empWage = empHrs * Emp_Rate_Per_Hr;
-        System.out.println("Emp Wage: " + empWage);
+        System.out.println("Total Employee Wage: "+ Total_empWage);
     }
 }
