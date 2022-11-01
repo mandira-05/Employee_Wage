d02e151cc1e32ca2b694b3f93633dd1d46d22a27
mandira-05 <mandira.sn.gowda@gmail.com>
2022-11-01 11:29:41 +0530

    Added total wage for each company by objects

--- Employee_wage.java
+++ Employee_wage.java
@@ -2,8 +2,20 @@ public class Employee_wage {
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
 
+    private final String companyName;
+    private final int Emp_Rate_Per_Hr;
+    private final int Num_of_working_day;
+    private final int Max_hrs_in_month;
+    private int Total_empWage;
 
-    public static void computeEmployeeWage(String companyName, int Emp_Rate_Per_Hr, int Num_of_working_day, int Max_hrs_in_month ) {
+    public Employee_wage(String companyName, int Emp_Rate_Per_Hr, int Num_of_working_day, int Max_hrs_in_month) {
+        this.companyName=companyName;
+        this.Emp_Rate_Per_Hr=Emp_Rate_Per_Hr;
+        this.Num_of_working_day=Num_of_working_day;
+        this.Max_hrs_in_month=Max_hrs_in_month;
+    }
+
+    public int computeEmployeeWage() {
         int empHrs = 0;
         int totalEmp_hrs = 0;
         int total_working_days = 0;
@@ -26,15 +38,25 @@ public class Employee_wage {
             totalEmp_hrs=totalEmp_hrs+empHrs;
             System.out.println("Day# " + total_working_days + " Employee hrs: " +empHrs );
         }
-        int Total_empWage = totalEmp_hrs * Emp_Rate_Per_Hr;
-        System.out.println("Total Employee Wage: "+ Total_empWage);
+        Total_empWage = totalEmp_hrs * Emp_Rate_Per_Hr;
+        return Total_empWage;
+    }
+
+    public String toString() {
+        return "Emp Wage per Month for company " +companyName+ " is : " +Total_empWage;
     }
 
     public static void main(String[] args) {
-        System.out.println("Details for Parle-G");
-        computeEmployeeWage("Parle-G",20,20,100);
 
-        System.out.println("Details for Tiger");
-        computeEmployeeWage("Tiger",22,22,120);
+
+        Employee_wage parle = new Employee_wage("Parle-G",20,20,100);
+        parle.computeEmployeeWage();
+        System.out.println(parle);
+
+        Employee_wage tiger = new Employee_wage("Tiger",22,22,120);
+        tiger.computeEmployeeWage();
+        System.out.println(tiger);
+
+
     }
 }
